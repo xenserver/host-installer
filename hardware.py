b9c3d46023d41b7fad1e6adde2db8aaaef235302
apeace <devnull@localhost>
2006-06-08 13:10:12 +0000

    Ignore framebuffer devices: CA-1926

---
+++ hardware.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+###
+# XEN CLEAN INSTALLER
+# Hardware discovery tools
+#
+# written by Andrew Peace
+# Copyright XenSource Inc. 2006
+
+# More of the hardware tools will be moved into here in future.
+
+# module => module list
+# if we discover x, actually load module_map[x]:
+module_map = {
+    # general:
+    'mptscsih'     : ['mptspi', 'mptscsih'],
+    'i810-tco'     : [],
+    'ide-scsi'     : ['ide-generic'],
+
+    # blacklist framebuffer drivers (we don't need them):
+    "arcfb"        : [],
+    "aty128fb"     : [],
+    "atyfb"        : [],
+    "radeonfb"     : [],
+    "cirrusfb"     : [],
+    "cyber2000fb"  : [],
+    "cyblafb"      : [],
+    "gx1fb"        : [],
+    "hgafb"        : [],
+    "i810fb"       : [],
+    "intelfb"      : [],
+    "kyrofb"       : [],
+    "i2c-matroxfb" : [],
+    "neofb"        : [],
+    "nvidiafb"     : [],
+    "pm2fb"        : [],
+    "rivafb"       : [],
+    "s1d13xxxfb"   : [],
+    "savagefb"     : [],
+    "sisfb"        : [],
+    "sstfb"        : [],
+    "tdfxfb"       : [],
+    "tridentfb"    : [],
+    "vfb"          : [],
+    "vga16fb"      : [],
+    }
