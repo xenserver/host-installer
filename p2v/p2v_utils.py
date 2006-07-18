5b03ec24a66294077286d512badc798af808a1f5
mnijmeijer <devnull@localhost>
2006-07-18 16:18:38 +0000

    CA-2701  replace customer password with SECRET in log files

--- p2v/p2v_utils.py
+++ p2v/p2v_utils.py
@@ -1,11 +1,15 @@
 import sys
+import re
 debug = False
 import xelogging
+
+def purge_xecli_password(message):
+    return re.sub("-p \S+", "-p SECRET", message)
 
 def trace_message(message):
     if debug:
         sys.stderr.write(message)
-    xelogging.log(message)
+    xelogging.log(purge_xecli_password(message))
         
 def is_debug():
     return debug
