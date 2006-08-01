f1580296150938380e87885b4bdcdae8c33fb982
avsm <devnull@localhost>
2006-08-01 14:13:18 +0000

    CA-2928: fix typo in installer

--- tui.py
+++ tui.py
@@ -1043,7 +1043,7 @@ def request_media(medianame):
     global screen
     
     button = ButtonChoiceWindow(screen, "Media Not Found",
-                                "Please insert the media labelled '%s' into your drive.  If the media is alredy present, then the installer was unable to locate it - please refer to your user guide, or %s technical support, for more information" % (medianame, COMPANY_NAME_SHORT),
+                                "Please insert the media labelled '%s' into your drive.  If the media is already present, then the installer was unable to locate it - please refer to your user guide, or %s technical support, for more information" % (medianame, COMPANY_NAME_SHORT),
                                 ['Retry', 'Cancel'], width=50)
 
     return button != "cancel"
