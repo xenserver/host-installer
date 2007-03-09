c757b7585a96cc5bac1a2984da290c18f613f143
Andrew Peace <andrew@xensource.com>
2007-03-09 16:52:30 +0000

    Correct grammar of dialog by adding a full-stop at the end of the final sentence.

--- tui/installer/screens.py
+++ tui/installer/screens.py
@@ -301,7 +301,7 @@ def linux_pack_warning(answers):
         "Hardware Wwarning",
         """You selected not to install the Linux pack but you system appears to be incapable of running Windows guests.
 
-If you have disabled hardware assist (HVM or AMD-V) support in your BIOS, please re-enable it to enable Windows guest support.  If your system does not support hardware assist, please install the Linux Pack from the second CD (see the user guide for more details)""",
+If you have disabled hardware assist (HVM or AMD-V) support in your BIOS, please re-enable it to enable Windows guest support.  If your system does not support hardware assist, please install the Linux Pack from the second CD (see the user guide for more details).""",
         ['Ok', 'Back']
         )
 
