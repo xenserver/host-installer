0f7c053fef02cfc54bd49484af3c58bee6248c70
Andrew Peace <andrew@xensource.com>
2007-07-25 16:52:34 +0100

    Add license and header comments to p2v/httpput and tui/installer/__init__.py.

--- tui/installer/__init__.py
+++ tui/installer/__init__.py
@@ -1,3 +1,15 @@
+# Copyright (c) 2005-2006 XenSource, Inc. All use and distribution of this 
+# copyrighted material is governed by and subject to terms and conditions 
+# as licensed by XenSource, Inc. All other rights reserved.
+# Xen, XenSource and XenEnterprise are either registered trademarks or 
+# trademarks of XenSource Inc. in the United States and/or other countries.
+
+###
+# XEN CLEAN INSTALLER
+# Installer TUI sequence definitions
+#
+# written by Andrew Peace
+
 import tui.installer.screens 
 import tui.progress
 import uicontroller
