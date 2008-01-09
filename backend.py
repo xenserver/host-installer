2755bb107f8021c10008de54feaf37b1a076e843
Simon Rowe <simon.rowe@eu.citrix.com>
2008-01-09 13:01:59 +0000

    CA-12490: install xen64 package if requested

--- backend.py
+++ backend.py
@@ -155,6 +155,10 @@ def getFinalisationSequence(ans):
         Task(setRootPassword, A(ans, 'mounts', 'root-password', 'root-password-type'), []),
         Task(setTimeZone, A(ans, 'mounts', 'timezone'), []),
         ]
+
+    if ans.has_key('install-xen64') and ans['install-xen64']:
+        seq.append(Task(installXen64, A(ans, 'mounts'), []))
+
     # on fresh installs, prepare the storage repository as required:
     if ans['install-type'] == INSTALL_TYPE_FRESH:
          seq += [
@@ -1001,6 +1005,38 @@ def writei18n(mounts):
     fd.write('LANG="en_US.UTF-8"\n')
     fd.write('SYSFONT="drdos8x8"\n')
     fd.close()
+
+def installXen64(mounts):
+    def find_devs():
+        devs = []
+        f = open('/proc/partitions')
+        f.readline()
+        f.readline()
+        for line in f.readlines():
+            col = line.split()
+            devs.append('/dev/'+col[3])
+        return devs
+
+    rpmdir = os.path.join('opt', 'xensource', 'RPMS')
+    if not os.path.exists(os.path.join(mounts['root'], rpmdir)):
+        return
+
+    # currently only contains xen64 RPM but cope with others
+    rpmlist = ["/%s/%s" % (rpmdir, f) for f in os.listdir(os.path.join(mounts['root'], rpmdir)) if f.endswith('.rpm')]
+    if len(rpmlist) > 0:
+        util.bindMount("/proc", "%s/proc" % mounts['root'])
+        util.bindMount("/sys", "%s/sys" % mounts['root'])
+        # grubby needs to access disk device files
+        dev_list = find_devs()
+        util.runCmd2(['tar', 'cf', '/tmp/dev.tar'] + dev_list)
+        util.runCmd2(['tar', 'xf', '/tmp/dev.tar', '-C', mounts['root']])
+        util.runCmd2(['chroot', mounts['root'], 'new-kernel-pkg', '--package', 'kernel-xen', '--install', '--multiboot=/boot/xen.gz', version.KERNEL_VERSION])
+        util.runCmd2(['chroot', mounts['root'], '/bin/rpm', '-ihv'] + rpmlist)
+        # cleanup /dev
+        for d in dev_list:
+            os.unlink(mounts['root'] + d)
+        util.umount("%s/proc" % mounts['root'])
+        util.umount("%s/sys" % mounts['root'])
 
 def getUpgrader(source):
     """ Returns an appropriate upgrader for a given source. """
