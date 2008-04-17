3a5bce58c735183c0d9ddbb5417e491b2abd10f0
Simon Rowe <simon.rowe@eu.citrix.com>
2008-04-17 15:09:24 +0100

    New functions so users do not have to manipulate continuous_logs

--- xelogging.py
+++ xelogging.py
@@ -55,7 +55,7 @@ def collectLogs(dst):
     os.system("ps axf >%s/processes-log" % dst)
     os.system("vgscan -P >%s/vgscan-log 2>&1" % dst)
 
-    # now, try to get the startup-log (it won't be in the same dstectory
+    # now, try to get the startup-log (it won't be in the same directory
     # most likely, but check in case):
     if not os.path.exists("%s/startup-log" % dst):
         # it didn't exist, so we need to try and fetch it -it ought to be in
@@ -68,6 +68,24 @@ def collectLogs(dst):
 
     # tar up contents
     os.system("tar -C %s -cjf %s/support.tar.bz2 %s" % (dst, dst, logs))
+
+def openLog(file):
+    if hasattr(file, 'name'):
+        # file object
+        continuous_logs.append(file)
+    else:
+        try:
+            f = open(file, 'w', 1)
+            continuous_logs.append(f)
+        except:
+            log("Error opening %s as a log output." % file)
+            return False
+    return True
+
+def closeLogs():
+    for fd in continuous_logs:
+        if not fd.name.startswith('<'):
+            fd.close()
 
 
 def main():
