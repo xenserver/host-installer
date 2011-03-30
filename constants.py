245b35918f21a0f0ef3f9e58fd0ee0d0599dcc86
Simon Rowe <simon.rowe@eu.citrix.com>
2011-03-30 16:16:57 +0100

    PR-1143: hide old Linux pack

--- constants.py
+++ constants.py
@@ -100,7 +100,7 @@ INVENTORY_FILE = "etc/xensource-inventory"
 BLOB_DIRECTORY = "var/xapi/blobs"
 
 MAIN_REPOSITORY_NAME = 'xs:main'
-INTERNAL_REPOS = ["xs:xenserver-transfer-vm", "xs:xenserver"]
+INTERNAL_REPOS = ["xs:xenserver-transfer-vm", "xs:xenserver", "xs:linux"]
 
 FIRSTBOOT_DATA_DIR = "etc/firstboot.d/data"
 INSTALLED_REPOS_DIR = "etc/xensource/installed-repos"
