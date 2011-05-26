36361159d5f81c5f003764dd8d3dee9645d1a6f2
Simon Rowe <simon.rowe@eu.citrix.com>
2011-05-26 11:59:49 +0100

    Parse args, bring up networking

--- bugtool.py
+++ bugtool.py
@@ -11,10 +11,55 @@ import sys
 import xcp.accessor
 import xcp.logger
 
+import netutil
 import product
 import util
 import xelogging
+from netinterface import *
 
+
+# Attempt to configure the network:
+def configureNetworking(device, config):
+    if device == 'all':
+        config = 'dhcp'
+    config_dict = None
+    try:
+        if config.startswith('static:'):
+            config_dict = {'gateway': None, 'dns': None, 'domain': None}
+            for el in config[7:].split(';'):
+                k, v = el.split('=', 1)
+                config_dict[k] = v
+            if 'dns' in config_dict:
+                config_dict['dns'] = config_dict['dns'].split(',')
+            assert 'ip' in config_dict and 'netmask' in config_dict
+    except:
+        pass
+
+    nethw = netutil.scanConfiguration()
+    netcfg = {}
+    for i in nethw.keys():
+        if (device == i or device == nethw[i].hwaddr) and config_dict:
+            netcfg[i] = NetInterface(NetInterface.Static, nethw[i].hwaddr,
+                                     config_dict['ip'], config_dict['netmask'],
+                                     config_dict['gateway'], config_dict['dns'],
+                                     config_dict['domain'])
+        else:
+            netcfg[i] = NetInterface(NetInterface.DHCP, nethw[i].hwaddr)
+
+    netutil.writeDebStyleInterfaceFile(netcfg, '/etc/network/interfaces')
+    netutil.writeResolverFile(netcfg, '/etc/resolv.conf')
+
+    if device == 'all':
+        for i in nethw.keys():
+            netutil.ifup(i)
+    elif device.startswith('eth'):
+        if nethw.has_key(device):
+            netutil.ifup(device)
+    else:
+        # MAC address
+        matching_list = filter(lambda x: x.hwaddr == device, nethw.values())
+        if len(matching_list) == 1:
+            netutil.ifup(matching_list[0].name)
 
 def bugtool(inst, dest_url):
     try:
@@ -45,26 +90,37 @@ def bugtool(inst, dest_url):
     finally:
         inst.unmount_root()
 
-def main(dest_url):
+def main(args):
     xcp.logger.openLog(sys.stdout)
     xelogging.openLog(sys.stdout)
+
+    dest_url = None
+    answer_device = 'all'
+    answer_config = 'dhcp'
+    init_network = False
+    reboot = False
+    
+    xelogging.log("Command line args: %s" % str(args))
+
+    for (opt, val) in args.items():
+        if opt in ['--answerfile_device', '--network_device']:
+            answer_device = val.lower()
+            init_network = True
+        elif opt == '--network_config':
+            answer_config = val.lower()
+        elif opt == "--reboot":
+            reboot = True
+        elif opt == "--dest":
+            dest_url = val
+
+    if init_network:
+        configureNetworking(answer_device, answer_config)
 
     # probe for XS installations
     insts = product.findXenSourceProducts()
     if len(insts) == 0:
         xcp.logger.log("No XenServer installations found.")
         return
-
-    # Locate destination dir
-    if not dest_url:
-        f = open('/proc/cmdline')
-        line = f.readline().strip()
-        cmd_args = line.split()
-        for arg in cmd_args:
-            if arg.startswith('dest='):
-                _, dest_url = arg.split('=', 1)
-                break
-        f.close()
 
     if not dest_url:
         xcp.logger.log("Destination directory not specified.")
@@ -74,7 +130,13 @@ def main(dest_url):
         xcp.logger.log(str(inst))
         bugtool(inst, dest_url)
 
+    return reboot
+
+
 if __name__ == "__main__":
-    main(None)
-    
-    # os.system('reboot')
+    f = open('/proc/cmdline')
+    args = map(lambda x: '--'+x, f.readline().strip().split())
+    f.close()
+    reboot = main(util.splitArgs(args))
+    if reboot:
+        os.system("reboot")
