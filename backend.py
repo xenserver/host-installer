68ef373c183feb1a903afda33cbe4ec7ce7b8bc9
Frediano Ziglio <frediano.ziglio@citrix.com>
2012-05-22 11:21:05 +0100

    CA-80444: create and fill disable-ipv6 file if IPv6 is not enabled

--- backend.py
+++ backend.py
@@ -1091,6 +1091,10 @@ def configureNetworking(mounts, admin_iface, admin_bridge, admin_config, hn_conf
         nfd.write("NETWORKING_IPV6=yes\n")
     else:
         nfd.write("NETWORKING_IPV6=no\n")
+        dv6fd = open("%s/etc/modprobe.d/disable-ipv6" % mounts["root"], "w")
+        dv6fd.write("alias ipv6 no\nalias net-pf-10 off\n")
+        dv6fd.close()
+        del dv6fd
     nfd.write("IPV6_AUTOCONF=no\n")
     if manual_hostname:
         nfd.write("HOSTNAME=%s\n" % hostname)
