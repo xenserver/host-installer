cf3b8ac1a8f4283e1ec89fdd69c976eb805b5153
Andrew Cooper <andrew.cooper3@citrix.com>
2012-11-23 16:16:21 +0000

    CA-67890 - Actually fix this bug.  (It has only taken a year to notice ...)

--- upgrade.py
+++ upgrade.py
@@ -233,7 +233,7 @@ class ThirdGenUpgrader(Upgrader):
         self.restore_list += [{'dir': 'etc/sysconfig/network-scripts/interface-rename-data/.from_install'}]
 
         # CA-67890: preserve root's ssh state
-        self.restore_list += [{'dir': '/root/.ssh'}]
+        self.restore_list += [{'dir': 'root/.ssh'}]
 
         # CA-82709: preserve networkd.db for Tampa upgrades
         self.restore_list.append(constants.NETWORK_DB)
