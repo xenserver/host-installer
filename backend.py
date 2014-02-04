cdd9652edbd6616b17abf253d1aed64fee599ff7
Frediano Ziglio <frediano.ziglio@citrix.com>
2014-02-04 11:07:33 +0000

    CP-7652: Error manual installing host with manual time
    timeutil is no more installed on dom0.
    Copy file in tmp directory to make it work again.

--- backend.py
+++ backend.py
@@ -458,7 +458,8 @@ def configureNTP(mounts, ntp_servers):
 
 def configureTimeManually(mounts, ui_package):
     # display the Set Time dialog in the chosen UI:
-    rc, time = util.runCmd2(['chroot', mounts['root'], 'timeutil', 'getLocalTime'], with_stdout = True)
+    shutil.copy('/usr/bin/timeutil', "%s/tmp/timeutil" % mounts['root'])
+    rc, time = util.runCmd2(['chroot', mounts['root'], '/tmp/timeutil', 'getLocalTime'], with_stdout = True)
     assert rc == 0
     answers = {}
     ui_package.installer.screens.set_time(answers, util.parseTime(time))
@@ -469,8 +470,9 @@ def configureTimeManually(mounts, ui_package):
                newtime.hour, newtime.minute)
         
     # chroot into the dom0 and set the time:
-    assert util.runCmd2(['chroot', mounts['root'], 'timeutil', 'setLocalTime', '%s' % timestr]) == 0
+    assert util.runCmd2(['chroot', mounts['root'], '/tmp/timeutil', 'setLocalTime', '%s' % timestr]) == 0
     assert util.runCmd2(['hwclock', '--utc', '--systohc']) == 0
+    os.unlink("%s/tmp/timeutil" % mounts['root'])
 
 
 def inspectTargetDisk(disk, existing, initial_partitions, preserve_first_partition, create_sr_part):
