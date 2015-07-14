583dde8780a49618ed4a78c48a73ca23a65e1d47
Ross Lagerwall <ross.lagerwall@citrix.com>
2015-07-14 12:51:03 +0000

    CA-175387: Unmount log partition at end of installation

--- backend.py
+++ backend.py
@@ -1156,6 +1156,8 @@ def umountVolumes(mounts, cleanup, force = False):
     util.umount(constants.EXTRA_SCRIPTS_DIR)
     if 'esp' in mounts:
         util.umount(mounts['esp'])
+    if 'logs' in mounts:
+        util.umount(mounts['logs'])
     util.umount(mounts['root'])
     cleanup = filter(filterCleanup, cleanup)
     return cleanup
