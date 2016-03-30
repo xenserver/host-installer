f7d8b6f894048806aa3db78564a74e7d1562517f
Ross Lagerwall <ross.lagerwall@citrix.com>
2016-03-30 14:31:11 +0100

    CA-204956: Handle conversion from scsi-* links to ata-* links
    If a disk device has moved from using scsi-* link to ata-* links, update
    the XAPI database and firstboot data to contain the correct links.
    
    If the device in the XAPI database is incorrect, then the upgrade
    plugin's safe2upgrade check may give incorect results, ultimately
    causing the partition layout to be upgraded even though there are VDIs
    on the local disk.

--- upgrade.py
+++ upgrade.py
@@ -517,6 +517,13 @@ class ThirdGenUpgrader(Upgrader):
                 util.runCmd2(['sed', '-i', '-e', "s#%s#%s#g" % (primary_disk, target_link),
                               os.path.join(mounts['root'], i)])
 
+        # handle the conversion of devices from scsi-* links to ata-* links
+        if primary_disk.startswith('/dev/disk/by-id/scsi-') and target_link.startswith('/dev/disk/by-id/ata-'):
+            for i in (os.path.join(constants.FIRSTBOOT_DATA_DIR, 'default-storage.conf'),
+                      constants.XAPI_DB):
+                util.runCmd2(['sed', '-i', '-e', "s#%s#%s#g" % (primary_disk, target_link),
+                              os.path.join(mounts['root'], i)])
+
 ################################################################################
 
 # Upgraders provided here, in preference order:
