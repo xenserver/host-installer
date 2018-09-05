26f61b7983b22f549bf9217a81e09c69cec50c68
Samuel Verschelde <stormi@laposte.net>
2018-09-05 11:03:17 +0200

    Add software RAID support.
    Originally-by: Nicolas Raynaud <github@nraynaud.com>
    
    Also adds support for it in the answerfile.
    
    the general form is :
    <raid device="md127">
      <disk>sda</disk>
      <disk>sdb</disk>
    </raid>
    
    avoid question and display waiting message when running mdadm
    
    Signed-off-by: Samuel Verschelde <stormi@laposte.net>
    
    When restoring from backup on a software RAID install, install grub on
    raid members instead of disk (which is a mdadm device)
    
    Signed-off-by: BenjiReis <benjamin.reis@vates.fr>
    
    Orig-commit: 6e168b43e0fd31cf3de9f578def1e10ba72ced35
    Orig-commit: 7557a0077a65d9353a56d7a21d054ceadc29b796
    Orig-commit: ee404b10b9809279600e0be121f88a675c0e2ef4
    Orig-commit: e6497d1c602691104a3d750692ffceedb25c1221
    Orig-commit: d33fe10fb1c36ee0326e48dbb8059a5fe42fa80b

--- answerfile.py
+++ answerfile.py
@@ -133,6 +133,7 @@ class Answerfile:
         results['preserve-settings'] = False
         results['backup-existing-installation'] = False
 
+        results.update(self.parseRaid())
         results.update(self.parseDisks())
         results.update(self.parseInterface())
         results.update(self.parseRootPassword())
@@ -291,6 +292,16 @@ class Answerfile:
                 address = util.URL(address)
 
             results['extra-repos'].append((rtype, address))
+        return results
+
+    def parseRaid(self):
+        results = {}
+        for raid_node in getElementsByTagName(self.top_node, ['raid']):
+            disk_device = normalize_disk(getStrAttribute(raid_node, ['device'], mandatory=True))
+            disks = [normalize_disk(getText(node)) for node in getElementsByTagName(raid_node, ['disk'])]
+            if 'raid' not in results:
+                results['raid'] = {}
+            results['raid'][disk_device] = disks
         return results
 
     def parseDisks(self):
