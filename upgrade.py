xenserver/host-installer

commit 648f0f93e996b2efe56718eb966d3c7d3d9782aa

CA-317754: Drop unneeded legacy disk/by-id symlink handling
Most of the legacy symlink handling is not needed anymore since upgrades
from 6.x are no longer supported. Remove it since in certain cases it
can cause issues by creating extra symlinks that disappear after
installation.

upgrade.py
@@ -474,33 +474,6 @@ class ThirdGenUpgrader(Upgrader):
             nfd.close()
             netutil.disable_ipv6_module(mounts["root"])
 
-        # handle the conversion of HP Gen6 controllers from cciss to scsi
-        primary_disk = self.source.getInventoryValue("PRIMARY_DISK")
-        target_link = diskutil.idFromPartition(target_disk) or target_disk
-        if 'cciss' in primary_disk and 'scsi' in target_link:
-            util.runCmd2(['sed', '-i', '-e', "s#%s#%s#g" % (primary_disk, target_link),
-                          os.path.join(mounts['root'], constants.FIRSTBOOT_DATA_DIR, 'default-storage.conf')])
-            util.runCmd2(['sed', '-i', '-e', "s#%s#%s#g" % (primary_disk, target_link),
-                          os.path.join(mounts['root'], constants.XAPI_DB)])
-
-        # handle the conversion of RAID devices from /dev/md_* to /dev/disk/by-id/md-uuid-*
-        if primary_disk.startswith('/dev/md_') and target_link.startswith('/dev/disk/by-id/md-uuid-'):
-            for i in (os.path.join(constants.FIRSTBOOT_DATA_DIR, 'default-storage.conf'),
-                      constants.XAPI_DB):
-                # First convert partitions from *pN to *-partN
-                util.runCmd2(['sed', '-i', '-e', "s#\(%s\)p\([[:digit:]]\+\)#\\1-part\\2#g" % primary_disk,
-                              os.path.join(mounts['root'], i)])
-                # Then conert from /dev/md_* to /dev/disk/by-id/md-uuid-*
-                util.runCmd2(['sed', '-i', '-e', "s#%s#%s#g" % (primary_disk, target_link),
-                              os.path.join(mounts['root'], i)])
-
-        # handle the conversion of devices from scsi-* links to ata-* links
-        if primary_disk.startswith('/dev/disk/by-id/scsi-') and target_link.startswith('/dev/disk/by-id/ata-'):
-            for i in (os.path.join(constants.FIRSTBOOT_DATA_DIR, 'default-storage.conf'),
-                      constants.XAPI_DB):
-                util.runCmd2(['sed', '-i', '-e', "s#%s#%s#g" % (primary_disk, target_link),
-                              os.path.join(mounts['root'], i)])
-
         # handle the conversion of devices from aacraid to smartpqi
         if primary_disk.startswith('/dev/disk/by-id/scsi-') and \
                 target_link.startswith('/dev/disk/by-id/scsi-') and \

startup/61-xenrt.rules
@@ -1,19 +1,0 @@
-# scsi compat links for ATA devices
-KERNEL=="sd*[!0-9]", PROGRAM="/usr/lib/udev/id_serial.sh %p $tempnode", RESULT=="?*", ENV{ID_SCSI_COMPAT}="$result", SYMLINK+="disk/by-id/scsi-$env{ID_SCSI_COMPAT}"
-KERNEL=="sd*[0-9]", ENV{ID_SCSI_COMPAT}=="?*", SYMLINK+="disk/by-id/scsi-$env{ID_SCSI_COMPAT}-part%n"
-
-# CA-58379 - because HP decided to crossport their gen6 controlers from cciss to hpsa, which breaks /dev/disk/by-id/* links
-ATTRS{vendor}=="0x103c", ATTRS{device}=="0x323a", ATTRS{subsystem_vendor}=="0x103c", ATTRS{subsystem_device}=="0x324?", ATTRS{class}=="0x010400", GOTO="CA-58379-start"
-
-GOTO="CA-58379-end"
-
-LABEL="CA-58379-start"
-# Problematic controller
-KERNEL=="sd*[!0-9]|sr*", ENV{ID_SERIAL}=="", IMPORT{program}="/lib/udev/scsi_id -g -x -s %p -d $tempnode"
-KERNEL=="cciss?c[0-9]d[0-9]", ENV{ID_SERIAL}=="", IMPORT{program}="/bin/sh -c 'echo ID_SERIAL=`/usr/lib/udev/cciss_id $$(echo %k|sed s,/,!,)`'", ENV{ID_BUS}="cciss"
-KERNEL=="sd*[!0-9]", ENV{ID_SERIAL}=="?*", SYMLINK+="disk/by-id/cciss-$env{ID_SERIAL}"
-
-KERNEL=="sd*[0-9]", IMPORT{parent}=="ID_*"
-KERNEL=="sd*[0-9]", ENV{ID_SERIAL}=="?*", SYMLINK+="disk/by-id/cciss-$env{ID_SERIAL}-part%n"
-
-LABEL="CA-58379-end"

startup/id_serial.sh
@@ -1,3 +1,0 @@
-#!/bin/bash
-
-/lib/udev/scsi_id.old -g -x -s $1 -d $2 -p0x80 | awk -F= '/ID_SERIAL/ {print $2}'

commit 2b8e429486fc564f069fb1db8a37c50a680849c7

CA-317754: Restore needed initialization code to fix upgrades
Commit 648f0f93e996 ("Drop unneeded legacy disk/by-id symlink
handling") accidentally removed some needed initialization. Restore the
code.

upgrade.py
@@ -472,6 +472,8 @@ class ThirdGenUpgrader(Upgrader):
             netutil.disable_ipv6_module(mounts["root"])
 
         # handle the conversion of devices from aacraid to smartpqi
+        primary_disk = self.source.getInventoryValue("PRIMARY_DISK")
+        target_link = diskutil.idFromPartition(target_disk) or target_disk
         if primary_disk.startswith('/dev/disk/by-id/scsi-') and \
                 target_link.startswith('/dev/disk/by-id/scsi-') and \
                 primary_disk != target_link: