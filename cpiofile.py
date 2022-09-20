xenserver/host-installer

commit bc9a0551cad08a33f10f2df485efc65a52620e55

CP-40676: Convert cpiofile.py to UTF-8
Signed-off-by: Ross Lagerwall <ross.lagerwall@citrix.com>

cpiofile.py
@@ -1,5 +1,4 @@
 #! /usr/bin/python
-# -*- coding: iso-8859-1 -*-
 #-------------------------------------------------------------------
 # cpiofile.py
 #-------------------------------------------------------------------
@@ -29,12 +28,12 @@
 #
 """Read from and write to cpio format archives.
 
-   Derived from Lars Gust‰bel's tarfile.py
+   Derived from Lars Gust√§bel's tarfile.py
 """
 
 version     = "0.1"
 __author__  = "Simon Rowe"
-__credits__ = "Lars Gust‰bel"
+__credits__ = "Lars Gust√§bel"
 
 #---------
 # Imports

commit 5f288e6ab1b2641e6aed182431dddacfbe673280

CP-40676: Specify utf-8 encoding explicitly
Commit bc9a0551cad0 ("CP-40676: Convert cpiofile.py to UTF-8") changed
cpiofile.py to UTF-8 but didn't specify the encoding. This is needed for
Python 2.x which doesn't default to UTF-8 (unlike Python 3.x).

Fixes: bc9a0551cad0 ("CP-40676: Convert cpiofile.py to UTF-8")
Signed-off-by: Ross Lagerwall <ross.lagerwall@citrix.com>

cpiofile.py
@@ -1,4 +1,5 @@
 #! /usr/bin/python
+# -*- coding: utf-8 -*-
 
 # SPDX-License-Identifier: MIT
 #