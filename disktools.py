xenserver/host-installer

commit 99bec34bfc47fd5ce88981d43269b3f03573c910

CA-391027: Mount devices specified by mount= only when needed
Using mount= option during installation allows to store and use
files from a specific device during installation.
It's possible to use a preserved partition in the installation
disk, however this currently makes the installer fail to upgrade
as the disk would be blocked so it cannot update partitioning
schema in the kernel.
Mount these devices only on needed paths.
Code for mounting these device is moved from S06mount script
(now not used) to installer code.

Signed-off-by: Frediano Ziglio <frediano.ziglio@cloud.com>

disktools.py
@@ -1295,3 +1295,78 @@ def getMdNodes():
     except IOError:
         pass
     return nodes
+
+
+class DeviceMounter:
+    class Mount:
+        def __init__(self, dev, mountpoint, options=None, fstype=None):
+            self.dev = dev
+            self.mountpoint = mountpoint
+            self.options = options
+            self.fstype = fstype
+
+        def __repr__(self):
+            return "Mount(%s, %s, %s, %s)" % (self.dev, self.mountpoint, self.fstype, self.options)
+
+        def mount(self):
+            util.mount(self.dev, self.mountpoint, self.options, self.fstype)
+
+        def umount(self):
+            util.umount(self.mountpoint)
+
+    mounts = []
+
+    @classmethod
+    def addMountPoints(cls, arg_mounts):
+        mounts = []
+        for arg in arg_mounts:
+            m = arg.split(':')
+            if len(m) < 2:
+                continue
+            dev = m[0]
+            fstype = None
+            options = None
+            if len(m) > 4:
+                raise Exception("Invalid syntax for --mount argument: %s" % arg)
+            if len(m) == 4:
+                fstype = m[1]
+                options = m[2]
+                mnt = m[3]
+            elif len(m) >= 3:
+                fstype = m[1]
+                mnt = m[2]
+            else:
+                mnt = m[1]
+            if dev.startswith("LABEL=") or dev.startswith("UUID="):
+                rc, out = util.runCmd2(['blkid', '-t', dev, '-o', 'device'], with_stdout=True)
+                if rc != 0:
+                    # for compatibility ignore the device
+                    continue
+                dev = out.rstrip()
+            elif dev.startswith("VG_"):
+                rc = util.runCmd2(LVMTool.LVCHANGE + ['-a', 'y', dev])
+                if rc != 0:
+                    # for compatibility ignore the device
+                    continue
+                dev = dev.replace("-", "--")
+                vg, lv, _ = (dev + "//").split("/", 2)
+                dev = "/dev/mapper/%s-%s" % (vg, lv)
+            mounts.append(cls.Mount(dev, mnt, options, fstype))
+        cls.mounts = mounts
+
+    def __init__(self):
+        self.mounts = self.__class__.mounts
+
+    def mount(self):
+        for m in self.mounts:
+            m.mount()
+
+    def __umount(self):
+        for m in self.mounts:
+            m.umount()
+
+    def __enter__(self):
+        self.mount()
+
+    def __exit__(self, type, value, traceback):
+        self.__umount()

backend.py
@@ -1226,6 +1226,9 @@ def installExtLinux(mounts, disk, location=constants.BOOT_LOCATION_MBR):
 # mounting and unmounting of various volumes
 
 def mountVolumes(primary_disk, boot_partnum, primary_partnum, logs_partnum, cleanup, target_boot_mode):
+    mounter = DeviceMounter()
+    mounter.mount()
+
     mounts = {'root': '/tmp/root',
               'boot': '/tmp/root/boot'}
 
@@ -1678,22 +1681,23 @@ def writei18n(mounts):
 def verifyRepos(sources, ui):
     """ Check repos are accessible """
 
-    for i in sources:
-        repo_good = False
+    with DeviceMounter():
+        for i in sources:
+            repo_good = False
 
-        if ui:
-            if tui.repo.check_repo_def((i['media'], i['address']), False) == tui.repo.REPOCHK_NO_ERRORS:
-               repo_good = True
-        else:
-            try:
-                repos = repository.repositoriesFromDefinition(i['media'], i['address'])
-                if len(repos) > 0:
+            if ui:
+                if tui.repo.check_repo_def((i['media'], i['address']), False) == tui.repo.REPOCHK_NO_ERRORS:
                     repo_good = True
-            except:
-                pass
+            else:
+                try:
+                    repos = repository.repositoriesFromDefinition(i['media'], i['address'])
+                    if len(repos) > 0:
+                        repo_good = True
+                except:
+                    pass
 
-        if not repo_good:
-            raise RuntimeError("Unable to access repository (%s, %s)" % (i['media'], i['address']))
+            if not repo_good:
+                raise RuntimeError("Unable to access repository (%s, %s)" % (i['media'], i['address']))
 
 def getUpgrader(source):
     """ Returns an appropriate upgrader for a given source. """

init
@@ -248,6 +248,6 @@ def main(args):
 
 
 if __name__ == "__main__":
-    reboot = main(util.splitArgs(sys.argv[1:], ('--console', '--map_netdev')))
+    reboot = main(util.splitArgs(sys.argv[1:], ('--console', '--map_netdev', '--mount')))
     if reboot:
         os.system("reboot")

install.py
@@ -128,6 +128,8 @@ def go(ui, args, answerfile_address, answerfile_script):
         elif opt == "--cc-preparations":
             constants.CC_PREPARATIONS = True
             results['network-backend'] = constants.NETWORK_BACKEND_BRIDGE
+        elif opt == "--mount":
+            disktools.DeviceMounter.addMountPoints(val)
 
     if boot_console and not serial_console:
         serial_console = boot_console
@@ -204,7 +206,8 @@ def go(ui, args, answerfile_address, answerfile_script):
         results['extra-repos'] += extra_repo_defs
         logger.log("Driver repos: %s" % str(results['extra-repos']))
 
-        scripts.run_scripts('installation-start')
+        with disktools.DeviceMounter():
+            scripts.run_scripts('installation-start')
 
         if parsing_except:
             raise parsing_except

startup/S06mount
@@ -1,78 +1,0 @@
-#!/bin/sh
-
-start()
-{
-  local oldifs="$IFS"
-
-  ret=0
-  for arg in `cat /proc/cmdline | sed -e 's/ $//; y/ /\n/'`; do
-    case "$arg" in
-      mount=*)
-        IFS=':'
-        set -- ${arg#mount=}
-        IFS="$oldifs"
-        [ -n "$2" ] || continue
-        device=$1
-        if [ -n "$4" ]; then
-            fstype=$2
-            opts=$3
-            mnt=$4
-        elif [ -n "$3" ]; then
-            fstype=$2
-            mnt=$3
-        else
-            mnt=$2
-        fi
-
-        case "$device" in
-          VG_*)
-          # Logical volume
-          lvchange -a y $device || continue
-          set -- `echo $device | tr / ' '`
-          vg=`echo $1 | sed -e 's/-/--/g'`
-          lv=`echo $2 | sed -e 's/-/--/g'`
-          device="/dev/mapper/$vg-$lv";;
-          LABEL=*)
-            device=`blkid -t $device -o device`;;
-        esac
-
-        [ -r $mnt ] || mkdir -p $mnt
-        [ -d $mnt ] || continue
-
-        # hang around a while for USB devices to settle
-        for n in `seq 1 5`; do
-          [ -b $device ] && break
-          sleep 5
-        done
-        if ! [ -b $device ]; then
-            continue
-        fi
-
-        if mount ${fstype:+-t $fstype} ${opts:+-o $opts} $device $mnt; then
-          echo "Mounted $device on $mnt"
-        fi;;
-    esac
-  done
-  return $ret
-}
-
-stop()
-{
-  return 0
-}
-
-
-RET=0
-case "$1" in
-  start)
-    start
-    RET=$?;;
-  stop)
-    stop
-    RET=$?;;
-  *)
-    echo "Usage: $0 start|stop"
-    RET=1
-esac
-
-exit $RET

startup/preinit
@@ -50,10 +50,6 @@ for arg in $args; do
       [ -z "$ramdisk_done" ] && /opt/xensource/installer/S05ramdisk start
       ramdisk_done=1
       copy=0;;
-    mount=*)
-      [ -z "$mount_done" ] && /opt/xensource/installer/S06mount start
-      mount_done=1
-      copy=0;;
     sshpassword=* | start=*)
       copy=0;;
   esac

util.py
@@ -128,14 +128,16 @@ def mount(dev, mountpoint, options=None, fstype=None):
     logger.log("Mounting %s to %s, options = %s, fstype = %s" % (dev, mountpoint, options, fstype))
 
     cmd = ['/bin/mount']
+    if type(options) == list:
+        options = ",".join(options)
     if options:
-        assert type(options) == list
+        assert type(options) == str
 
     if fstype:
         cmd += ['-t', fstype]
 
     if options:
-        cmd += ['-o', ",".join(options)]
+        cmd += ['-o', options]
 
     cmd.append(dev)
     cmd.append(mountpoint)

commit e121926397630f5af4deb947b2dc311b10f7e4db

CA-392916: Create directory for mount= option
This fixes a regression introduced by commit 99bec34bfc
"CA-391027: Mount devices specified by mount= only when needed",
code was always attempting to create the directory before
mounting.

Signed-off-by: Frediano Ziglio <frediano.ziglio@cloud.com>

disktools.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 import constants
+import errno
 import re, subprocess, types, os, time
 from pprint import pprint
 from copy import copy, deepcopy
@@ -1285,6 +1286,12 @@ class DeviceMounter:
             return "Mount(%s, %s, %s, %s)" % (self.dev, self.mountpoint, self.fstype, self.options)
 
         def mount(self):
+            try:
+                os.makedirs(self.mountpoint, mode=0o755)
+            except OSError as e:
+                if e.errno != errno.EEXIST:
+                    raise
+
             util.mount(self.dev, self.mountpoint, self.options, self.fstype)
 
         def umount(self):