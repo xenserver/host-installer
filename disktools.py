3ec75969d31d25d1b66b2b386227686f99d6c255
Frediano Ziglio <frediano.ziglio@cloud.com>
2024-11-13 13:47:15 +0000

    Allows automatic conversion of some partitions to GPT
    Newer Gdisks returns different GUID for Linux, handle them.
    
    Signed-off-by: Frediano Ziglio <frediano.ziglio@cloud.com>

--- disktools.py
+++ disktools.py
@@ -942,6 +942,7 @@ class GPTPartitionTool(PartitionToolBase):
     # This GUID is "Microsoft basic data partition" but for historic
     # reasons we use for Linux partitions.
     ID_LINUX        = "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7"
+    ID_LINUX2       = "0FC63DAF-8483-4772-8E79-3D69D8477DE4"
     ID_LINUX_LVM    = "E6D6D379-F507-44C2-A23C-238F2A3DF928"
     ID_EFI_BOOT     = "C12A7328-F81F-11D2-BA4B-00A0C93EC93B"
     ID_BIOS_BOOT    = "21686148-6449-6E6F-744E-656564454649"
@@ -950,6 +951,7 @@ class GPTPartitionTool(PartitionToolBase):
     GUID_to_type_code = {
         ID_LINUX_SWAP:   '8200',
         ID_LINUX:        '0700',
+        ID_LINUX2:       '0700',
         ID_LINUX_LVM:    '8e00',
         ID_EFI_BOOT:     'ef00',
         ID_BIOS_BOOT:    'ef02',
