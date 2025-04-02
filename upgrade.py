df620321ded849662fb585b9020c8310df8de1ee
Yann Dirson <yann.dirson@vates.tech>
2025-04-02 11:37:36 +0200

    upgrade: restore saved firewall-port plugin state
    Signed-off-by: Yann Dirson <yann.dirson@vates.tech>
    (cherry picked from commit 65f54b18bbfebc32cb01427ae124a0873e266fad)

--- upgrade.py
+++ upgrade.py
@@ -683,6 +683,9 @@ class ThirdGenUpgrader(Upgrader):
         restore_list += ['etc/stunnel/xapi-pool-ca-bundle.pem', {'dir': 'etc/stunnel/certs-pool'}]
         restore_list += ['etc/stunnel/xapi-stunnel-ca-bundle.pem', {'dir': 'etc/stunnel/certs'}]
 
+        # XAPI firewall-port plugin
+        restore_list += ['etc/sysconfig/iptables']
+
         return restore_list
 
     completeUpgradeArgs = ['mounts', 'installation-to-overwrite', 'primary-disk', 'backup-partnum', 'logs-partnum', 'net-admin-interface', 'net-admin-bridge', 'net-admin-configuration']
