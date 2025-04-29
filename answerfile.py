64b1fdb7657606f9ba40c92e7fb7d0711aa66601
Deli Zhang <deli.zhang@cloud.com>
2025-04-29 08:42:25 +0000

    CA-409996: Fix KeyError 'target-platform' issue
    Define this key early than within answerfile parsing.
    
    Signed-off-by: Deli Zhang <deli.zhang@cloud.com>

--- answerfile.py
+++ answerfile.py
@@ -220,7 +220,6 @@ class Answerfile:
             if bl not in ['' , 'grub2']:
                 raise AnswerfileException("Unsupported bootloader '%s'" % bl)
 
-        results['target-platform'] = ''
         nodes = getElementsByTagName(self.top_node, ['target-platform'])
         if len(nodes) > 0:
             results['target-platform'] = getText(nodes[0])
