5aa76fb1c10fe8e6a0b03aede5bf5d910b299405
Gerald Elder-Vass <gerald.elder-vass@cloud.com>
2025-05-23 11:51:52 +0000

    CP-54461: Add SWRAID restore functionality
    Signed-off-by: Gerald Elder-Vass <gerald.elder-vass@cloud.com>

--- restore.py
+++ restore.py
@@ -128,6 +128,33 @@ def restoreFromBackup(backup, progress=lambda x: ()):
                     if m:
                         bootlabel = m.group(1)
 
+            if diskutil.is_raid(disk):
+                backend.waitForSWRAIDSync(disk)
+
+                # umount and re-mount partitions to flush the cache
+                # umount all partitions first before re-mounting
+                util.umount(mounts['esp'])
+                dest_fs.unmount()
+
+                # Restart the SW RAID device
+                physical_disks = diskutil.getSWRAIDDevices(disk)
+                if len(physical_disks) == 0:
+                    raise RuntimeError("Failed to identify physical devices of SWRAID device: %s" % disk)
+
+                assembleCommand = ["mdadm", "--assemble", "--run", disk, physical_disks[0]]
+                if len(physical_disks) == 2:
+                    assembleCommand.append(physical_disks[1])
+
+                diskutil.stopSWRAID(disk)
+                if util.runCmd2(assembleCommand) != 0:
+                    raise RuntimeError("Failed to re-assemble SWRAID device: %s" % disk)
+
+                dest_fs = util.TempMount(restore_partition, 'restore-dest-')
+                os.makedirs(mounts['esp'])
+                util.mount(boot_device, mounts['esp'])
+            else:
+                physical_disks = [disk]
+
             # prepare extra mounts for installing bootloader:
             util.bindMount("/dev", "%s/dev" % dest_fs.mount_point)
             util.bindMount("/sys", "%s/sys" % dest_fs.mount_point)
@@ -135,7 +162,7 @@ def restoreFromBackup(backup, progress=lambda x: ()):
 
             branding = util.readKeyValueFile(os.path.join(backup_fs.mount_point, constants.INVENTORY_FILE))
             branding['product-brand'] = branding['PRODUCT_BRAND']
-            backend.setEfiBootEntry(mounts, disk, boot_partnum, constants.INSTALL_TYPE_RESTORE, branding)
+            backend.writeBootEntries(True, mounts, physical_disks, boot_partnum, constants.INSTALL_TYPE_RESTORE, branding)
 
             # restore bootloader configuration
             dst_file = boot_config.src_file.replace(backup_fs.mount_point, dest_fs.mount_point, 1)
