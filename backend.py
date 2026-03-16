a8964ae0d47c7fff46526929b6b5f8d7bd9fabd6
Ross Lagerwall <ross.lagerwall@citrix.com>
2026-03-16 16:53:27 +0000

    CP-311882: Remove the safe mode option
    The safe mode boot option does not produce a usable system since
    "no-hap" prevents booting any VMs (shadow is compiled out) and
    "no-mmcfg" prevents the use of PCIe devices. Furthermore, "no-numa" is
    not a valid Xen option. Rather than fixing safe mode, just remove it
    since is has not generally been useful for customers - they should just
    contact support if a XenServer system fails to boot the installer.
    
    Signed-off-by: Ross Lagerwall <ross.lagerwall@citrix.com>

--- backend.py
+++ backend.py
@@ -1053,12 +1053,9 @@ def prepFallback(mounts, primary_disk, primary_partnum):
 
 def buildBootLoaderMenu(mounts, xen_version, xen_kernel_version, boot_config, serial, boot_serial, host_config, primary_disk, disk_label_suffix):
     short_version = kernelShortVersion(xen_kernel_version)
-    common_xen_params = "dom0_mem=%dM,max:%dM" % ((host_config['dom0-mem'],) * 2)
-    common_xen_unsafe_params = "watchdog dom0_max_vcpus=1-%d" % host_config['dom0-vcpus']
-    safe_xen_params = ("nosmp noreboot noirqbalance no-mce no-bootscrub "
-                       "no-numa no-hap no-mmcfg max_cstate=0 "
-                       "nmi=ignore allow_unsafe")
-    xen_mem_params = "crashkernel=512M,below=4G"
+    common_xen_params = ("dom0_mem=%dM,max:%dM " % ((host_config['dom0-mem'],) * 2) +
+                         "watchdog dom0_max_vcpus=1-%d " % host_config['dom0-vcpus'] +
+                         "crashkernel=512M,below=4G")
 
     # CA-103933 - AMD PCI-X Hypertransport Tunnel IOAPIC errata
     rc, out = util.runCmd2(['lspci', '-n'], with_stdout=True)
@@ -1081,7 +1078,7 @@ def buildBootLoaderMenu(mounts, xen_version, xen_kernel_version, boot_config, se
         common_kernel_params += " rd.auto"
 
     e = bootloader.MenuEntry(hypervisor="/boot/xen.efi",
-                             hypervisor_args=' '.join([common_xen_params, common_xen_unsafe_params, xen_mem_params, "console=vga vga=mode-0x0311"]),
+                             hypervisor_args=' '.join([common_xen_params, "console=vga vga=mode-0x0311"]),
                              kernel="/boot/vmlinuz-%s-xen" % short_version,
                              kernel_args=' '.join([common_kernel_params, kernel_console_params, "console=tty0 quiet vga=785 splash plymouth.ignore-serial-consoles"]),
                              initrd="/boot/initrd-%s-xen.img" % short_version, title=MY_PRODUCT_BRAND,
@@ -1093,7 +1090,7 @@ def buildBootLoaderMenu(mounts, xen_version, xen_kernel_version, boot_config, se
         xen_serial_params = "%s console=%s,vga" % (serial.xenFmt(), serial.port)
 
         e = bootloader.MenuEntry(hypervisor="/boot/xen.efi",
-                                 hypervisor_args=' '.join([xen_serial_params, common_xen_params, common_xen_unsafe_params, xen_mem_params]),
+                                 hypervisor_args=' '.join([xen_serial_params, common_xen_params]),
                                  kernel="/boot/vmlinuz-%s-xen" % short_version,
                                  kernel_args=' '.join([common_kernel_params, "console=tty0", kernel_console_params]),
                                  initrd="/boot/initrd-%s-xen.img" % short_version, title=MY_PRODUCT_BRAND+" (Serial)",
@@ -1102,14 +1099,6 @@ def buildBootLoaderMenu(mounts, xen_version, xen_kernel_version, boot_config, se
         boot_config.append("xe-serial", e)
         if boot_serial:
             boot_config.default = "xe-serial"
-        e = bootloader.MenuEntry(hypervisor="/boot/xen.efi",
-                                 hypervisor_args=' '.join([safe_xen_params, common_xen_params, xen_serial_params]),
-                                 kernel="/boot/vmlinuz-%s-xen" % short_version,
-                                 kernel_args=' '.join(["earlyprintk=xen", common_kernel_params, "console=tty0", kernel_console_params]),
-                                 initrd="/boot/initrd-%s-xen.img" % short_version, title=MY_PRODUCT_BRAND+" in Safe Mode",
-                                 root=constants.rootfs_label%disk_label_suffix)
-        e.entry_format = Grub2Format.XEN_BOOT
-        boot_config.append("safe", e)
 
     e = bootloader.MenuEntry(hypervisor="", hypervisor_args="", kernel="/boot/memtest86+x64.efi",
                             kernel_args="",
@@ -1119,7 +1108,7 @@ def buildBootLoaderMenu(mounts, xen_version, xen_kernel_version, boot_config, se
     boot_config.append("memtest", e)
 
     e = bootloader.MenuEntry(hypervisor="/boot/xen-fallback.efi",
-                             hypervisor_args=' '.join([common_xen_params, common_xen_unsafe_params, xen_mem_params]),
+                             hypervisor_args=common_xen_params,
                              kernel="/boot/vmlinuz-fallback",
                              kernel_args=' '.join([common_kernel_params, kernel_console_params, "console=tty0"]),
                              initrd="/boot/initrd-fallback.img",
@@ -1129,7 +1118,7 @@ def buildBootLoaderMenu(mounts, xen_version, xen_kernel_version, boot_config, se
     boot_config.append("fallback", e)
     if serial:
         e = bootloader.MenuEntry(hypervisor="/boot/xen-fallback.efi",
-                                 hypervisor_args=' '.join([xen_serial_params, common_xen_params, common_xen_unsafe_params, xen_mem_params]),
+                                 hypervisor_args=' '.join([xen_serial_params, common_xen_params]),
                                  kernel="/boot/vmlinuz-fallback",
                                  kernel_args=' '.join([common_kernel_params, "console=tty0", kernel_console_params]),
                                  initrd="/boot/initrd-fallback.img",
